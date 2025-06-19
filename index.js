0cccd02c43b906124e6674da59702860eeb8b2df
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-06-19 23:54:15 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const hamburgerIcon = document.getElementById("hamburger-icon");
-const mobileMenu = document.getElementById("mobile-menu");
-const closeMenu = document.getElementById("close-menu");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerIcon = document.getElementById("hamburger-icon");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const closeMenu = document.getElementById("close-menu");
 
-hamburgerIcon.addEventListener("click", () => {
-  mobileMenu.classList.add("active");
-});
+  if (hamburgerIcon && mobileMenu && closeMenu) {
+    hamburgerIcon.addEventListener("click", () => {
+      mobileMenu.classList.add("active");
+    });
 
-closeMenu.addEventListener("click", () => {
-  mobileMenu.classList.remove("active");
+    closeMenu.addEventListener("click", () => {
+      mobileMenu.classList.remove("active");
+    });
+  }
 });
